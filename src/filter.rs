9f462f0dc38f5fc398474cecd2cc226feb36079f
canberkkoc <canberkkoc@gmail.com>
2023-12-11 21:50:11 +0300

    Prototype created

--- src/filter.rs
+++ src/filter.rs
@@ -1,7 +1,28 @@
 use fuzzy_matcher::skim::SkimMatcherV2;
 use fuzzy_matcher::FuzzyMatcher;
+use std::fs;
 
-pub fn filter_entries(app_entries: Vec<&str>, search_for: &str) {
+use freedesktop_desktop_entry::{default_paths, DesktopEntry, Iter, PathSource};
+
+
+pub fn get_desktop_entries() -> Vec<String>{
+    let mut vectore = Vec::new();
+    for path in Iter::new(default_paths()) {
+        let path_src = PathSource::guess_from(&path);
+        if let Ok(bytes) = fs::read_to_string(&path) {
+            if let Ok(entry) = DesktopEntry::decode(&path, &bytes) {
+                if let Some(entriname) = entry.name(None){
+                    vectore.push(entriname.to_string());
+                };
+            }
+        }
+    }
+    vectore
+}
+
+
+
+pub fn filter_entries(app_entries: Vec<String>, search_for: &str) {
     let matcher = SkimMatcherV2::default();
     for i in app_entries {
         if let Some(point) = matcher.fuzzy_match(&i, search_for) {
@@ -9,3 +30,6 @@ pub fn filter_entries(app_entries: Vec<&str>, search_for: &str) {
         }
     }
 }
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-mod gui;
+use filter::filter_entries;
 
+mod gui;
+mod filter;
 pub fn main() {
+    let vectorius = filter::get_desktop_entries();
+    filter_entries(vectorius, "frfox");
     let _ = gui::gui_run();
 }
