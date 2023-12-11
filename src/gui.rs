96ec6fe27edc33ca203b73a5dc34abf9a7edc949
canberkkoc <canberkkoc@gmail.com>
2023-12-11 19:49:24 +0300

    Minor immediate changes

--- src/gui.rs
+++ src/gui.rs
@@ -1,7 +1,7 @@
 use iced::alignment::Alignment;
 use iced::theme::Theme;
-use iced::widget::{text_input, row};
-use iced::{window, Command, Application, executor, Subscription, subscription, Event};
+use iced::widget::{row, text_input};
+use iced::{executor, subscription, window, Application, Command, Event, Subscription};
 use iced::{Element, Length, Settings};
 
 pub fn gui_run() -> iced::Result {
@@ -20,7 +20,7 @@ pub fn gui_run() -> iced::Result {
 #[derive(Clone)]
 struct Searcher {
     input_value: String,
-    window_id: String,
+    input_id: text_input::Id,
 }
 
 #[derive(Debug, Clone)]
@@ -31,33 +31,41 @@ enum Message {
 
 impl Application for Searcher {
     type Message = Message;
+    type Executor = executor::Default;
+    type Theme = iced::Theme;
 
+    type Flags = ();
     fn new(_flags: ()) -> (Self, Command<Message>) {
-        (Searcher {input_value: "".to_string(), window_id: "123".to_string()}, Command::none())
+        (
+            Searcher {
+                input_value: "".to_string(),
+                input_id: text_input::Id::new("1"),
+            },
+            Command::none(),
+        )
     }
 
     fn title(&self) -> String {
         String::from("")
     }
 
-    fn update(&mut self, message: Message) -> Command<Message>{
+    fn update(&mut self, message: Message) -> Command<Message> {
         match message {
             Message::InputChanged(value) => {
                 self.input_value = value;
                 Command::none()
-            },
+            }
             Message::WindowOpened(Event::Window(window::Event::Focused)) => {
-                text_input::focus(text_input::Id::new(self.window_id.clone()))
-
-            },
-            _ => Command::none()
+                text_input::focus(self.input_id.clone())
+            }
+            _ => Command::none(),
         }
     }
 
     fn view(&self) -> Element<Message> {
         row![text_input("Search an app...", &self.input_value)
             .on_input(Message::InputChanged)
-            .id(text_input::Id::new(self.window_id.clone()))
+            .id(self.input_id.clone())
             .size(20)]
         .align_items(Alignment::Center)
         .height(Length::Shrink)
@@ -72,9 +80,4 @@ impl Application for Searcher {
     fn theme(&self) -> Theme {
         Theme::Dark
     }
-
-    type Executor = executor::Default;
-
-    type Theme = iced::Theme;
-    type Flags = ();
 }
