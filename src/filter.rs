a040abdbe67a7294b336cc588ab5ba767346785b
Canberk Koç <canberkkoc@gmail.com>
2023-12-12 18:30:32 +0300

    3 filter allsystem+user desktop entries with input from search bar (#10)
    * Prototype created
    
    * Change if to match to be more rustacean
    
    * Error handling plays introduced
    
    * Sorting added
    
    * Approach changed to not borrow variables

--- src/filter.rs
+++ src/filter.rs
@@ -1,11 +1,56 @@
+use freedesktop_desktop_entry::{default_paths, DesktopEntry, Iter};
 use fuzzy_matcher::skim::SkimMatcherV2;
 use fuzzy_matcher::FuzzyMatcher;
+use std::fs;
 
-pub fn filter_entries(app_entries: Vec<&str>, search_for: &str) {
-    let matcher = SkimMatcherV2::default();
-    for i in app_entries {
-        if let Some(point) = matcher.fuzzy_match(&i, search_for) {
-            println!("{:?} {:?}", point, i);
+#[derive(Debug, Clone, Default)]
+pub struct FilteredEntry {
+    name: String,
+    exec_path: String,
+    icon_path: String,
+}
+
+pub fn get_desktop_entries() -> Vec<FilteredEntry> {
+    let mut entry_vector = Vec::new();
+    for path in Iter::new(default_paths()) {
+        if let Ok(bytes) = fs::read_to_string(&path) {
+            if let Ok(entry) = DesktopEntry::decode(&path, &bytes) {
+                let mut tmp_obj = FilteredEntry::default();
+                match entry.name(None) {
+                    Some(val) => tmp_obj.name = val.to_string(),
+                    _ => (),
+                }
+                match entry.exec() {
+                    Some(val) => tmp_obj.exec_path = val.to_string(),
+                    _ => (),
+                }
+                match entry.icon() {
+                    Some(val) => tmp_obj.icon_path = val.to_string(),
+                    _ => (),
+                }
+                entry_vector.push(tmp_obj)
+            }
         }
+    }
+    entry_vector
+}
+
+pub fn filter_entries(
+    app_entries: Vec<FilteredEntry>,
+    search_for: &str,
+    limit_entry_count: i32,
+) -> Vec<(i64, FilteredEntry)> {
+    let mut entry_vector: Vec<(i64, FilteredEntry)> = Vec::new();
+    let matcher = SkimMatcherV2::default();
+
+    for entry in app_entries {
+        if let Some(point) = matcher.fuzzy_match(&entry.name, search_for) {
+            entry_vector.push((point, entry));
+        }
+    }
+    entry_vector.sort_by(|a, b| b.0.cmp(&a.0));
+    match limit_entry_count {
+        0 => entry_vector,
+        val => entry_vector[0..usize::try_from(val).unwrap()].to_vec(),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+mod filter;
 mod gui;
 
+use filter::{filter_entries, get_desktop_entries};
+use gui::gui_run;
+
 pub fn main() {
-    let _ = gui::gui_run();
+    println! {"{:?}",filter_entries(get_desktop_entries(), "go", 0)};
+    match gui_run() {
+        Ok(()) => (),
+        error => println!("{:?}", error),
+    }
 }
