b964bbb274401fe6f8733cee2a916c2807977624
canberkkoc <canberkkoc@gmail.com>
2023-12-12 18:16:35 +0300

    Approach changed to not borrow variables

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-mod gui;
 mod filter;
+mod gui;
 
 use filter::{filter_entries, get_desktop_entries};
 use gui::gui_run;
 
 pub fn main() {
-    println!{"{:?}",filter_entries(get_desktop_entries(), "go", 2)};
-    match gui_run(){
+    println! {"{:?}",filter_entries(get_desktop_entries(), "go", 0)};
+    match gui_run() {
         Ok(()) => (),
-        error => println!("{:?}", error)
+        error => println!("{:?}", error),
     }
 }
